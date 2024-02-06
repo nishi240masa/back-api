e89300068b4a3eadc366f09fbff8fc51a6256bd5
nishi240masa <masa2401106@gmail.com>
2024-02-06 19:44:33 +0900

    Refactor database connection and API endpoint

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST || process.env.DB_,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST || process.env.DB_,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool.connect(err => {
@@ -25,18 +24,15 @@ pool.connect(err => {
   }
 });
 
-
-// recordsテーブルを作成するSQLクエリ
 const createTableQuery = `
   CREATE TABLE IF NOT EXISTS records (
-    id SERIAL PRIMARY KEY,          
-    dateKey VARCHAR(255) NOT NULL,  
-    salary INT NOT NULL,             
-    hours INT NOT NULL           
+    id SERIAL PRIMARY KEY,
+    dateKey VARCHAR(255) NOT NULL,
+    salary INT NOT NULL,
+    hours INT NOT NULL
   );
 `;
 
-// テーブルの作成を実行
 pool.query(createTableQuery, (err, result) => {
   if (err) {
     console.error('テーブルの作成エラー:', err);
@@ -45,30 +41,27 @@ pool.query(createTableQuery, (err, result) => {
   }
 });
 
-// レコードを挿入するAPIエンドポイント
 app.post('/api/records', async (req, res) => {
   const { dateKey, salary, hours } = req.body;
 
   console.log('dateKey:', dateKey);
-    console.log('salary:', salary);
-    console.log('hours:', hours);
+  console.log('salary:', salary);
+  console.log('hours:', hours);
 
   try {
-    // レコードを挿入し、挿入されたデータを返す
     const result = await pool.query(
       'INSERT INTO records (dateKey, salary, hours) VALUES ($1, $2, $3) RETURNING *',
       [dateKey, salary, hours]
     );
 
     console.log('レコードが正常に挿入されました');
-    res.status(201).json({ message: 'レコードが正常に作成されました', data: result.rows[0] });
+    res.status(201).json({ success: true, data: result.rows[0] });
   } catch (error) {
     console.error('レコードの挿入エラー:', error);
-    res.status(500).json({ message: '内部サーバーエラー' });
+    res.status(500).json({ success: false, error: error.message });
   }
 });
 
-// サーバーを指定されたポートで起動
 app.listen(port, () => {
   console.log(`サーバーがポート${port}で実行されています`);
 });
