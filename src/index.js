0b48fcc0f326089a4d05b320246eab590e090ae7
nishi240masa <masa2401106@gmail.com>
2024-02-06 22:41:34 +0900

    Refactor response object in API endpoint

--- src/index.js
+++ src/index.js
@@ -102,9 +102,9 @@ app.get('/api/total', async (req, res) => {
       const totalHours = total.totalHours;
   
 
-      res.json({ success: true, data: { salary: total[0], hours: total[1] } });
+      res.json({ success: true, data: total });
       console.log('全体の合計値:', total.totalSalary, total.totalHours);
-      console.log('tota:', total);
+      console.log('total:', total);
       console.log('total[0]:', total[0]);
         console.log('total[1]:', total[1]);
       console.log('全体の合計値の取得に成功しました');
