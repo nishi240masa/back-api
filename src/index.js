20370f23057f6620cc12640ed4e3b4ed663ad2d8
nishi240masa <masa2401106@gmail.com>
2024-02-06 17:18:52 +0900

    Fix port variable typo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const PORT = 3010;
+const port = process.env.PORT || 3000;;
 
 app.use(express.json());
 app.use(cors());
